4aca0646a07ff852e2d22864168286038ecf9126
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2023-01-25 10:36:22 +0100

    feat: added validation for password

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 "use strict";
+const submitButton = document.querySelector("button");
+submitButton.addEventListener("click", function checkPasswordValidity(e) {
+	const passwordInput = document.querySelector("input.password");
+	const confirmPasswordInput = document.querySelector(
+		"input.confirm-password"
+	);
+	if (passwordInput.value !== confirmPasswordInput.value) {
+		e.preventDefault();
+		passwordInput.classList.add("invalid");
+		confirmPasswordInput.classList.add("invalid");
+		document.querySelector("span.error").textContent =
+			"Passwords do not match";
+	}
+});
